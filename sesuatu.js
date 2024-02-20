dffa7c61dc2958af559fa7a46754efed6cd66b25
nfal001 <abdurahman.nfal@gmail.com>
2024-02-20 19:51:03 +0700

    new challenges init

--- sesuatu.js
+++
@@ -1,6 +0,0 @@
-const alphabetPosition = (text)=>{
-    const z = "abcdefghijklmnopqrstuvwxyz".split('')
-    return text.toLowerCase().split("").filter((e) => z.indexOf(e) > -1).map((a)=>z.indexOf(a) +1).join(" ")
-}
-
-console.log(alphabetPosition("The sunset sets at twelve o' clock."))
